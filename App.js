d6104248cab0e629e586406329eb6a11776479c5
shareefa-rn <147628835+shareefa-rn@users.noreply.github.com>
2023-11-02 12:15:31 +0000

    custom hook tested

--- App.js
+++ App.js
@@ -9,12 +9,23 @@ import {NavigationContainer} from '@react-navigation/native';
 import {DrawerScreen} from './src/containers';
 import {PersistanceHelper} from './src/helpers';
 import {EventRegister} from 'react-native-event-listeners';
+import useFectch from './src/helpers/useFetch';
 
 const App = () => {
-  const login = () => {};
-  const nav = () => {};
+  const [data] = useFectch('https://jsonplaceholder.typicode.com/todos');
+  const Fetch = () => {
+    return (
+      <>
+        {data &&
+          data.map(item => {
+            console.log('custome hook==' + item.title);
+          })}
+      </>
+    );
+  };
   return (
     <NavigationContainer>
+      <Fetch />
       <DrawerScreen />
     </NavigationContainer>
   );
